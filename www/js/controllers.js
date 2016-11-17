cec393423fe92c53eeb860b79f4fb56697357900
Ji Ma <ji.ma@ndkey.com.cn>
2016-11-17 16:52:52 +0800

    add baidumap and heatmap

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,MapService) {
+  $scope.map = null;
+  $scope.heatmapOverlay = null;
+  $scope.init = function(){
+    // init Map obj
+    $scope.map = new BMap.Map("container");          // 创建地图实例
+    var point = new BMap.Point(116.418261, 39.921984);
+    $scope.map.centerAndZoom(point, 15);             // 初始化地图，设置中心点坐标和地图级别
+    $scope.map.enableScrollWheelZoom(); // 允许滚轮缩放
+
+    // init Heatmap obj
+    $scope.heatmapOverlay = new BMapLib.HeatmapOverlay({"radius":20});
+    $scope.map.addOverlay($scope.heatmapOverlay);
+  };
+  $scope.showHeatData = function(){
+    if(null==$scope.heatData){
+      var points = MapService.getHeatData();
+      $scope.heatmapOverlay.setDataSet({data:points,max:100});
+    }
+    $scope.heatmapOverlay.show();
+  };
+  $scope.hideHeatData = function(){
+    $scope.heatmapOverlay.hide();
+  };
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
